8bd4c0e65619832c99ac9dd3c1e8f968cd67d9e1
kemaloncel3535@outlook.com <kemaloncel3535@outlook.com>
2022-05-07 10:43:34 +0300

    update tasks

--- src/validations/Tasks.js
+++ src/validations/Tasks.js
@@ -1,15 +1,34 @@
 const Joi = require('joi');
+const Mongoose = require("mongoose");
 
 const createValidation = Joi.object({
     title: Joi.string().required().min(3),
     project_id: Joi.string().min(8).required(),
     section_id: Joi.string().min(8).required(),
+    description: Joi.string().min(8),
+    assigned_to: Joi.string().min(8),
+    due_date: Joi.date(),
+    statuses: Joi.array(),
+    order: Joi.number(),
+    isCompleted: Joi.boolean(),
+    comments: Joi.array(),
+    media: Joi.array(),
+    sub_tasks: Joi.array(),
 })
 
 const updateValidation = Joi.object({
     title: Joi.string().min(3),
+    project_id: Joi.string().min(8),
+    section_id: Joi.string().min(8),
+    description: Joi.string().min(8),
+    assigned_to: Joi.string().min(8),
     due_date: Joi.date(),
-
+    statuses: Joi.array(),
+    order: Joi.number(),
+    isCompleted: Joi.boolean(),
+    comments: Joi.array(),
+    media: Joi.array(),
+    sub_tasks: Joi.array(),
 })
 
 module.exports = {
